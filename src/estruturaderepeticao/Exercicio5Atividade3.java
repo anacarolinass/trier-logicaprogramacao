bb14198b5b481cdd94870dd73357ad65b27a1680
Alexson Felipe <alexfelipesilveira@gmail.com>
2022-05-09 13:28:02 -0300

    Exercicio estrutura de repetição 5

--- src/estruturaderepeticao/Exercicio5Atividade3.java
+++ src/estruturaderepeticao/Exercicio5Atividade3.java
@@ -7,36 +7,30 @@ public class Exercicio5Atividade3 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Digite um valor impar: ");
 		int valorImpar = sc.nextInt();
 		
 		for (int i = 1; i <= valorImpar; i++) {
-			
 			for (int j = 1; j <= valorImpar; j++) {
 				
-				for (int j2 = 0; j2 < valorImpar; j2++) {
-					
-				}
-				
-				if (i == 1) {
+				if (i == j) {
 					System.out.print(j);
-					for (int j2 = 1; j2 <= valorImpar - 1; j2++) {
-						System.out.print("*");
-						
-					}
-					System.out.print("\n");
 				}else {
-					
-					if (i > 1) {
+					if ((i == 1) || (i == valorImpar)) {
 						System.out.print("*");
-						System.out.print(i);
+					}else {
+						if((j == 1) || (j == valorImpar)) {
+							System.out.print("*");
+						}else {
+							System.out.print(" ");
+						}
 					}
 				}
 			}
-			System.out.print("\n");
+			System.out.println();
 		}
-		
+
 	}
 
 }
--- src/estruturaderepeticao/test.java
+++ src/estruturaderepeticao/test.java
@@ -11,10 +11,24 @@ public class test {
 		System.out.print("Digite um valor impar: ");
 		int valorImpar = sc.nextInt();
 		
-		for (int i = 0; i < valorImpar; i++) {
-			for (int j = 0; j < valorImpar; j++) {
+		for (int i = 1; i <= valorImpar; i++) {
+			for (int j = 1; j <= valorImpar; j++) {
 				
+				if (i == j) {
+					System.out.print(j);
+				}else {
+					if ((i == 1) || (i == valorImpar)) {
+						System.out.print("*");
+					}else {
+						if((j == 1) || (j == valorImpar)) {
+							System.out.print("*");
+						}else {
+							System.out.print(" ");
+						}
+					}
+				}
 			}
+			System.out.println();
 		}
 
 	}
